7b3be031954540f22e26f7622e59067f72715d16
k-nakaie <102917326+k-nakaie@users.noreply.github.com>
2023-01-23 23:21:28 +0900

    １章課題修正

---
+++ 2-2/index.php
@@ -0,0 +1,31 @@
+<?php
+// ダブルコーテーションで囲われているので文字列
+$message = "Hello World!";
+var_dump($message);
+echo '<br>';
+$x = 8;
+$y = 5.55;
+var_dump($x);
+echo '<br>';
+var_dump($y);
+$emp = NULL;
+var_dump($emp);
+echo '<br>';
+$en_words = "Hello World";
+var_dump($en_words);
+echo '<br>';
+$ja_words = "こんにちわ";
+var_dump($ja_words);
+echo '<br>';
+
+echo 'API：Application Programming Interface
+          アプリケーションとプログラムをつなぐもの。
+          ソフトウェアやアプリの一部を外部に公開することで、第三者が開発したソフトウェアと
+          機能を共有できるようにしてくれるもの。
+      Subversion：著名なあオープンソースのバージョン管理システムの一つ。
+                　複数の開発者が共同で一つのソフトウェアを開発する際などに、ソースコードや
+                ドキュメントを管理するのに用いられる。CVSを元に開発されている。
+                サーバ上の保存領域でファイルを集中的に管理する集中型バージョン管理システム。';
+
+?>
+
