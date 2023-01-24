37adbc0b51c8a8e20a462da62e7266b3931cf285
k-nakaie <102917326+k-nakaie@users.noreply.github.com>
2023-01-24 23:44:55 +0900

    2-12課題提出
    index.php
    result.php
    IT用語

---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+    $my_name = $_POST['my_name'];
+    $goods = $_POST['goods'];
+    $count = $_POST['count'];
+?>
+<p>お名前：<?php echo $my_name; ?></p>
+<p>ご希望景品：<?php echo $goods; ?></p>
+<p>個数：<?php echo $count; ?></p>
